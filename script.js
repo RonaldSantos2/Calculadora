ff875ae00d781b254c7a6bb418dc0f3123e36f30
Ronald Santos2.git <ronaldocabofrio@icloud.com.git>
2023-05-23 15:54:35 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toDisplay(value) {
+    document.getElementById('display').value += value;
+}
+function clearDisplay() {
+    document.getElementById('display').value = "";
+}
+function calculate() {
+    let expr = document.getElementById('display').value;
+    let result = eval(expr)
+    document.getElementById('display'). value = result;
+}
